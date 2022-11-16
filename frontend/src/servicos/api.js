92bd1375e7acc8b969332053a0f693046f785ec4
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-11-16 17:01:44 -0300

    Adaptação na API

--- frontend/src/servicos/api.js
+++ frontend/src/servicos/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// Conexão via IP para conexão com dispositivos na mesma rede
+// const api = axios.create({
+//   baseURL: 'http://192.168.0.113:3003',
+// });
+
 const api = axios.create({
-  baseURL: 'http://192.168.0.113:3003',
+  baseURL: 'http://localhost:3003',
 });
 
 export default api;
